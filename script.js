b9116068a2811e45a98c4cad21d286180be85659
rncraven94 <neilcraven2013@gmail.com>
2023-11-13 20:40:14 -0500

    script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 const generateUser = document.getElementById("user-generate");
 
 const UserRoot = function (username) {
-  console.log(this);
+  this.username = username;
 };
 
 //generate user is a temporary function for now that will allow me to understand how to create objects
 generateUser.addEventListener("click", function () {
-  new UserRoot("Robert");
+  const newUser = new UserRoot("Robert");
+  console.log(newUser);
   // console.log();
 });
