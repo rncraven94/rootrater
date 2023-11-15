b8156bf9640cc447cf6b7daa056c15124cba181a
rncraven94 <neilcraven2013@gmail.com>
2023-11-14 22:41:47 -0500

    index.html

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 "use strict";
 
 const generateUser = document.getElementById("user-generate");
+const userObjects = {};
 
 const UserRoot = function (username) {
   this.username = username;
 };
 
 //generate user is a temporary function for now that will allow me to understand how to create objects
-generateUser.addEventListener("click", function () {
-  const newUser = new UserRoot("Robert");
-  console.log(newUser);
+generateUser.addEventListener("click", function (e) {
+  e.preventDefault();
+  // get value from the form
+  const usernameInput = document.getElementById("usernameInput").ariaValueMax;
+  const newUser = new UserRoot(usernameInput);
+  userObjects[usernameInput] = newUser;
+  console.log(userObjects[usernameInput]);
   // console.log();
 });
