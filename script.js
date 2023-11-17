412e0d921db24566bcbd3ac74b584aa7b63e5a57
rncraven94 <neilcraven2013@gmail.com>
2023-11-16 23:20:40 -0500

    index.html

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 const generateUser = document.getElementById("user-generate");
 const userObjects = {};
 
-const UserRoot = function (username) {
+const UserRoot = function (username, password) {
   this.username = username;
+  this.password = password;
 };
 
 //generate user is a temporary function for now that will allow me to understand how to create objects
@@ -12,7 +13,8 @@ generateUser.addEventListener("click", function (e) {
   e.preventDefault();
   // get value from the form
   const usernameInput = document.getElementById("usernameInput").value;
-  const newUser = new UserRoot(usernameInput);
+  const userPassword = document.getElementById("password").value;
+  const newUser = new UserRoot(usernameInput, userPassword);
   userObjects[usernameInput] = newUser;
   console.log(userObjects[usernameInput]);
   console.log(userObjects);
